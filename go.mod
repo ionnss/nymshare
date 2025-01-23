bf95b45df229c737d21dd7818636103f752c39ba
ionnss <39104500+ionnss@users.noreply.github.com>
2025-01-23 11:01:37 -0300

    Update go.mod and go.sum to include new dependencies for internationalization; modify routes to serve static files correctly. Enhance index.html with language selector and i18n support for dynamic content translation.

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module nymshare
 go 1.23.4
 
 require github.com/gorilla/mux v1.8.1
+
+require (
+	github.com/nicksnyder/go-i18n/v2 v2.5.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- routes/routes.go
+++ routes/routes.go
@@ -8,8 +8,8 @@ import (
 
 func ConfigureRoutes(r *mux.Router) {
 	// Serve static files and assets
-	fileServer := http.FileServer(http.Dir("."))
-	r.PathPrefix("/static/").Handler(fileServer)
+	fileServer := http.FileServer(http.Dir("static"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
 
 	// Pages
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
